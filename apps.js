a7c6f950b27749b18b04a61e0ddac4bebe9ac322
yoann <yochaps@gmail.com>
2022-02-20 11:57:09 +0100

    finnih

--- apps.js
+++ apps.js
@@ -2,11 +2,35 @@ const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
 const dataUppercase = dataLowercase.toLocaleUpperCase();
 const dataNumbers = "0123456789";
 const dataSymbols = "@#&§!?%^¨$*`£<>+=/:.;,éèçà°";
+const rangeValue = document.getElementById("password-length");
+const passwordOutput = document.getElementById("password-output");
 
 function generatePassword() {
-    console.log("hello");
+    let data = [];
+    let password = "";
+    if (lowercase.checked) data.push(...dataLowercase);
+    if (uppercase.checked) data.push(...dataUppercase);
+    if (numbers.checked) data.push(...dataNumbers);
+    if (symbols.checked) data.push(...dataSymbols);
+
+    if (data.length === 0) {
+        alert("Veuillez sélectionner des critéres");
+        return;
+    }
+    for (i = 0; i < rangeValue.value; i++) {
+        password += data[Math.floor(Math.random() * data.length)];
+        console.log(password);
+    }
+    passwordOutput.value = password;
+    passwordOutput.select();
+    document.execCommand("copy");
+
+    generateButton.textContent = "copié !";
+
+    setTimeout(() => {
+        generateButton.textContent = "Générer mot de passe"
+    }, 2000)
 }
 
 generateButton.addEventListener("click", generatePassword);
-
-console.log(dataUppercase);
+lowercase;
