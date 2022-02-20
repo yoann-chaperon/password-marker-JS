2c0692002c736caa0b5a1008d8db937f92b4dde9
yoann <yochaps@gmail.com>
2022-02-20 10:46:41 +0100

    first commit

---
+++ apps.js
@@ -0,0 +1,12 @@
+const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
+const dataUppercase = dataLowercase.toLocaleUpperCase();
+const dataNumbers = "0123456789";
+const dataSymbols = "@#&§!?%^¨$*`£<>+=/:.;,éèçà°";
+
+function generatePassword() {
+    console.log("hello");
+}
+
+generateButton.addEventListener("click", generatePassword);
+
+console.log(dataUppercase);
